d027c045810be0e663b7eb1ea956c0cb52c212fe
digitalcriminals <89371864+DIGITALCRIMINALS@users.noreply.github.com>
2023-04-11 16:08:01 +0100

    Added rate_limit test

--- test.py
+++ test.py
@@ -13,14 +13,23 @@ async def main():
         if isinstance(user, ultima_scraper_api.user_types):
             return await user.get_posts()
 
+    async def rate_limit_enabler(authed: ultima_scraper_api.auth_types):
+        # Set the URL and number of requests to send
+        url = "https://onlyfans.com/api2/v2/init"
+        urls = [url] * 10000
+        response = await authed.session_manager.bulk_json_requests(urls)
+        are_all_dicts = all(isinstance(item, dict) for item in response)
+        assert are_all_dicts == True
+
     api = ultima_scraper_api.select_api("OnlyFans")
     authed = await authenticate(api)
+
     posts = await get_all_posts(authed)
     if posts:
         for post in posts:
             if not post.text:
                 continue
-            author = await post.get_author()
+            author = post.get_author()
             print(f"User: {author.username}\nContent: {post.text}")
             break
     await api.close_pools()
