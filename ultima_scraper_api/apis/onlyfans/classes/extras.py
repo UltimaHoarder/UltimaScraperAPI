63d8eeac3732fdf05b866be0260aafb62d412213
digitalcriminals <89371864+DIGITALCRIMINALS@users.noreply.github.com>
2023-05-31 20:02:01 +0100

    updated paid_content link

--- ultima_scraper_api/apis/onlyfans/classes/extras.py
+++ ultima_scraper_api/apis/onlyfans/classes/extras.py
@@ -144,7 +144,7 @@ class endpoint_links(object):
         self.list_posts_api = self.list_posts(identifier)
         self.archived_posts = f"https://onlyfans.com/api2/v2/users/{identifier}/posts/archived?limit={global_limit}&offset={global_offset}&order=publish_date_desc"
         self.archived_stories = f"https://onlyfans.com/api2/v2/stories/archive/?limit=100&offset=0&order=publish_date_desc"
-        self.paid_api = f"https://onlyfans.com/api2/v2/posts/paid?{global_limit}&offset={global_offset}"
+        self.paid_api = f"https://onlyfans.com/api2/v2/posts/paid?limit={global_limit}&offset={global_offset}"
         self.pay = f"https://onlyfans.com/api2/v2/payments/pay"
         self.subscribe = f"https://onlyfans.com/api2/v2/users/{identifier}/subscribe"
         self.like = f"https://onlyfans.com/api2/v2/{identifier}/{identifier2}/like"
