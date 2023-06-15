ad9fc8046e3e02cd18350a5941e9e71a70c2413c
digitalcriminals <89371864+DIGITALCRIMINALS@users.noreply.github.com>
2023-06-15 09:31:36 +0100

    added "caching"

--- ultima_scraper_api/apis/auth_streamliner.py
+++ ultima_scraper_api/apis/auth_streamliner.py
@@ -3,6 +3,33 @@ from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     import ultima_scraper_api
+
+
+class CacheStats:
+    def __init__(self) -> None:
+        self.processed_at: datetime | None = None
+        self.delay_in_seconds = 3600 * 1  # hour in seconds * hour
+        self.released_at: datetime | None = None
+
+    def activate(self):
+        self.processed_at = datetime.utcnow()
+        self.released_at = self.processed_at + timedelta(seconds=self.delay_in_seconds)
+
+    def is_released(self):
+        status = True
+        if self.released_at:
+            if datetime.utcnow() < self.released_at:
+                status = False
+            else:
+                self.processed_at = self.released_at = None
+        return status
+
+
+class Cache:
+    def __init__(self) -> None:
+        self.paid_content = CacheStats()
+
+
 class StreamlinedAuth:
     def __init__(self, authenticator: "ultima_scraper_api.authenticator_types") -> None:
         self.authenticator = authenticator
